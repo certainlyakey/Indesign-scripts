2a6f42eede80dfac46d19a6c1964847b1cdfc434
certainlyakey <sashabe@yandex.ru>
2015-10-20 18:53:41 +0300

    added scripts for resizing from center

---
+++ enlargefromcenter.jsx
@@ -0,0 +1,11 @@
+var currentPoint = app.activeWindow.transformReferencePoint;
+app.activeWindow.transformReferencePoint = AnchorPoint.CENTER_ANCHOR;
+
+app.selection[0].geometricBounds = [
+	app.selection[0].geometricBounds[0] - 1, //ul y
+	app.selection[0].geometricBounds[1] - 1, //ul x
+	app.selection[0].geometricBounds[2] + 1, //lr y
+	app.selection[0].geometricBounds[3] + 1 //lr x
+];
+
+app.activeWindow.transformReferencePoint = currentPoint;
