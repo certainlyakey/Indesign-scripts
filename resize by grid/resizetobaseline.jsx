47cc10ce9c953d2fc3e76429170f166f5dc701ee
certainlyakey <sashabe@yandex.ru>
2015-10-20 18:34:16 +0300

    refactored resize to baseiine scripts
    removed a couple of bugs

--- resize by grid/resizetobaseline.jsx
+++ resize by grid/resizetobaseline.jsx
@@ -3,28 +3,19 @@
 var myDocument = app.activeDocument;
 var baseline = myDocument.gridPreferences.baselineDivision;
 
-var oldHeight = app.selection[0].geometricBounds[2];
-var newHeight = app.selection[0].geometricBounds[2];
-
-var linesNumber = oldHeight / baseline;
-var difference = 0;
-
-if (linesNumber === parseInt(linesNumber, 10)) { //if integer
-	newHeight = newHeight + baseline;
+var lowerY = Math.round(app.selection[0].geometricBounds[2]);
+var linesNumber = lowerY / baseline;
+var higherLinesNumber = Math.ceil(linesNumber);
+if (Number(higherLinesNumber) === Number(linesNumber)) {
+	lowerY += baseline;
 } else {
-	var decimal = linesNumber - Math.floor(linesNumber);
-	if (decimal < 0.5) {
-		difference = -(baseline*decimal);
-	} else {
-		difference = baseline*(1-decimal);
-	}
-	newHeight = newHeight + difference;
+	lowerY = baseline * higherLinesNumber;
 }
 
 app.selection[0].geometricBounds = [
 	app.selection[0].geometricBounds[0],
 	app.selection[0].geometricBounds[1],
-	newHeight,
+	lowerY,
 	app.selection[0].geometricBounds[3]
 ];
 
--- resize by grid/shrinktobaseline.jsx
+++ resize by grid/shrinktobaseline.jsx
@@ -1,23 +1,18 @@
 var myDocument = app.activeDocument;
 var baseline = myDocument.gridPreferences.baselineDivision;
 
-var oldHeight = app.selection[0].geometricBounds[2];
-var newHeight = app.selection[0].geometricBounds[2];
-
-var linesNumber = oldHeight / baseline;
-var difference = 0;
-
-if (linesNumber === parseInt(linesNumber, 10)) { //if integer
-	newHeight = newHeight - baseline;
+var lowerY = Math.round(app.selection[0].geometricBounds[2]);
+var linesNumber = lowerY / baseline;
+var lowerLinesNumber = Math.floor(linesNumber);
+if (Number(lowerLinesNumber) === Number(linesNumber)) {
+	lowerY -= baseline;
 } else {
-	var decimal = linesNumber - Math.floor(linesNumber);
-		difference = baseline*decimal;
-	newHeight = newHeight - difference;
+	lowerY = baseline * lowerLinesNumber;
 }
-
 app.selection[0].geometricBounds = [
 	app.selection[0].geometricBounds[0],
 	app.selection[0].geometricBounds[1],
-	newHeight,
+	lowerY,
 	app.selection[0].geometricBounds[3]
 ];
+
